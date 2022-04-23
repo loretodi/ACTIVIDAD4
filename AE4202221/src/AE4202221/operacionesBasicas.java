33083387c2bb7ddf150885afdcee81134bbde09a
loreto <hlc05956@educastur.es>
2022-04-23 20:14:32 +0200

    Version 1

--- AE4202221/src/AE4202221/operacionesBasicas.java
+++ AE4202221/src/AE4202221/operacionesBasicas.java
@@ -3,27 +3,34 @@ import java.util.Scanner;
 public class operacionesBasicas {
 
 	public static void main(String[] args) {
-		// Introducir 2 numeros enteros positivos
-		// Realizar las 4 operaciones basicas matematicas (suma, resta, multiplicacion y division)
-		// Visualizar los resultados
+		/** Actividad evaluable n4  */
+		/** Introducir 2 números enteros positivos */
+  		/** Realizar las 4 operaciones básicas matemáticas (suma, resta, multiplicación y división) sin el uso de métodos */ 
+		/** Visualizar sus resultados. */
 		
+		/** @author Maria Loreto Diaz Fernandez */
+		/** @version Version1*/
+
+		/** Definicion de variables de trabajo */
 		int a, b, suma, resta , multiplicacion, division = 0;
 
+		/** Introduccion de datos */
 		Scanner s = new Scanner( System.in );
 	    System.out.println( "Introduce dos números: ");
 	    a = s.nextInt();
 	    b = s.nextInt();
-		
+
+	    /** Calculos */
 	    suma = a + b;
 	    resta = a - b;
 		multiplicacion = a * b;
 		division = a / b;
-	    
+		
+	    /** Visualizar los resultados */
         System.out.println( "La suma de a y b es: " + suma );
 	    System.out.println( "La resta de a y b es: " + resta );
 	    System.out.println( "La multiplicacion de a y b es: " + multiplicacion );
 	    System.out.println( "La division de a y b es: " + division );
-
 	}
 
 }
